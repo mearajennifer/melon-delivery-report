f4842c53efa8f8c76151c94a732a5c8888561c12
Jennifer Meara <jenniferlmeara@gmail.com>
2018-06-21 05:35:46 +0000

    Added function instead of hard coding

--- produce_summary.py
+++ produce_summary.py
@@ -1,43 +1,27 @@
-print("Day 1")
-the_file = open("um-deliveries-20140519.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+def print_report(report_lst):
+    """
+    Takes in list of report names, prints out deliveries
+    """
+    day = 0
+    for report in report_lst:
+        day += 1
+        print("Day {}".format(day))
 
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
+        the_file = open(report)
 
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+        for line in the_file:
+            line = line.rstrip()
+            words = line.split('|')
 
+            melon = words[0]
+            count = words[1]
+            amount = words[2]
 
-print("Day 2")
-the_file = open("um-deliveries-20140520.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+            print("Delivered {} {}s for total of ${}".format(
+                count, melon, amount))
 
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+    the_file.close()
 
 
-print("Day 3")
-the_file = open("um-deliveries-20140521.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+report_lst = ["um-deliveries-20140519.txt", "um-deliveries-20140520.txt", "um-deliveries-20140521.txt"]
+print_report(report_lst)
